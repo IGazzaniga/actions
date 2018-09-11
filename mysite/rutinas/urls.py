846009d3b04fee12ab46678cdc4cba5daf664c7c
IGazzaniga <ignagazzaniga@hotmail.com.ar>
2018-09-10 21:31:23 -0300

    Se añade BootstrapDjango

---
+++ mysite/rutinas/urls.py
@@ -0,0 +1,12 @@
+"""Rutinas urls."""
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
+from rutinas.views import index_view #Importamos las vistas
+
+
+
+urlpatterns = [
+    url(r'^index$', index_view, name="index"), 
+]
--- mysite/rutinas/views.py
+++ mysite/rutinas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index_view(request):
+    from django import forms
+    class LoginForm(forms.Form):
+        usuario = forms.CharField(label='Usuario', max_length=100)
+
+    template = "rutinas/base.html"
+    context = { "form" : LoginForm() }
+    return render( request, template, context )
