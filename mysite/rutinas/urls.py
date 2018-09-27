eeeaf3a0d0d7be85770fc5f1cca3e3b832429518
stegeri <stegerivan95@gmail.com>
2018-09-27 20:00:59 -0300

    se modifico rutinas

--- mysite/rutinas/urls.py
+++ mysite/rutinas/urls.py
@@ -3,10 +3,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
-from rutinas.views import index_view #Importamos las vistas
+from rutinas.views import index_view, rutina_view #Importamos las vistas
 
 
 
 urlpatterns = [
-    url(r'^$', index_view, name="rutinas"), 
+    url(r'^$', index_view, name="rutinas"),
+    url(r'^rutinaAlumno', rutina_view, name="rutinaAlumno"),
 ]
--- mysite/rutinas/views.py
+++ mysite/rutinas/views.py
@@ -7,3 +7,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index_view(request):
     return render(request, "rutinas/inicio-alumno.html")
+    
+@login_required
+def rutina_view(request):
+    return render(request, "rutinas/rutina.html")
